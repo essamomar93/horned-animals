d4b0bdbeb59ba8c31303ff5919d60ef3a8db3d8e
essam omar <essam.93@hotmail.com>
2021-09-05 17:40:32 +0300

    lab 01

---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Main extends Component {
+    render() {
+        return (
+            <>
+                <h2>Unicorn Hollow</h2>
+                <img src='https://process.filepicker.io/APHE465sSbqvbOIStdwTyz/rotate=deg:exif/resize=fit:crop,height:283,width:472/output=quality:80,compress:true,strip:true,format:jpg/cache=expiry:max/https://cdn.filestackcontent.com/1KfnS1kETJC8bdgBcizm'  alt='' title='Unicorn Hollow'/>
+                <p> In this one time course, students will learn about unicorn myths and one-horned beast legends from around the world, and how the creature we consider a unicorn morphed into its current form. </p>
+                <br />
+                <h2>Elasmotherium</h2>
+                <img src='https://www.ancient-origins.net/sites/default/files/field/image/Siberian-unicorns.jpg'  alt='' title='Elasmotherium'/> 
+                <p>Elasmotherium, also known as the Giant Rhinoceros or the Giant Siberian Unicorn, is an extinct species of rhino that lived in the Eurasian area in the Late Pliocene and Pleistocene eras. They have been documented from 2.6 million years ago, but the most recent fossils come from around 29,000 years ago. </p>
+            </>
+        )
+    }
+}
+
+export default Main
